6f983a86867eb3e03345275b2e8cb81e8df2112d
Muhammad <148522477+magasoov@users.noreply.github.com>
2024-04-07 10:47:46 +0300

    Add files via upload

---
+++ configure.py
@@ -0,0 +1,6 @@
+config = {
+	'name': 'magaasovbot_bot',
+	'token': '6820864694:AAFCNCaAIjtxq3PQd64-BHr2_Be320Mtzi4',
+	'tokenqiwi': '1e9001d8898964492163378fdc8f299d',
+	'phoneqiwi': '+79643000996'
+}
